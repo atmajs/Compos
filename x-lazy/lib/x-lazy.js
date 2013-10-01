955746b2cad1d27ff22239660593444570ee65f3
Alexander Kit <source.kit@gmail.com>
2013-10-01 15:20:11 +0200

    [dist] :lazy compo

---
+++ x-lazy/lib/x-lazy.js
@@ -0,0 +1,44 @@
+(function(mask){
+	
+	if (mask == null) 
+		mask = atma.mask;
+	
+	
+	mask.registerHandler(':lazy', Compo({
+		
+		_deferredNodes: null,
+		onRenderStart: function(model, ctx, container){
+			
+			this._deferredNodes = this.nodes;
+			
+			this.nodes = null;
+			this.model = model;
+			this.ctx = ctx;
+			this.container = container;
+			this.placeholder = document.createComment('');
+			
+			container.appendChild(this.placeholder);
+		},
+		
+		lazyShow: function(){
+			this.lazyShow = function(){};
+			
+			var fragment = mask.render(this._deferredNodes, this.model, this.ctx, null, this),
+				that = this;
+			
+			if (this.ctx.async) {
+				this.ctx.done(function(fragment){
+					that._appendLazy(fragment);
+				});
+			}
+			
+			that._appendLazy(fragment);
+			that.emitIn('domInsert');
+		},
+		
+		_appendLazy: function(fragment){
+			this.placeholder.parentNode.insertBefore(fragment, this.placeholder);
+		}
+	}));
+	
+}(typeof mask === 'undefined' ? null : mask));
